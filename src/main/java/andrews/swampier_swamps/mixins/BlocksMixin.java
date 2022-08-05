ddbd5870cd1360e8c3360e181290a116f536b708
GiantLuigi4 <49770992+GiantLuigi4@users.noreply.github.com>
2022-08-05 18:10:35 -0400

    fix unsafe code to forcefully set a final boolean pretty sure the solution was to just make a local variable, but I did other stuff as well to make it just a bit more future proof

--- src/main/java/andrews/swampier_swamps/mixins/BlocksMixin.java
+++ src/main/java/andrews/swampier_swamps/mixins/BlocksMixin.java
@@ -1,12 +1,11 @@
 package andrews.swampier_swamps.mixins;
 
 import andrews.swampier_swamps.util.TheUnsafeHelper;
+import cpw.mods.modlauncher.api.INameMappingService;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -15,23 +14,35 @@ import sun.misc.Unsafe;
 import java.lang.reflect.Field;
 
 @Mixin(Blocks.class)
-public class BlocksMixin
-{
-    @Shadow @Final public static final Block DRIED_KELP_BLOCK = null;
-
-    @Inject(method = "<clinit>", at = @At("TAIL"))
-    private static void postStaticInit(CallbackInfo ci)
-    {
-        Unsafe theUnsafe = TheUnsafeHelper.getTheUnsafe();
-        try
-        {
-            Field f = BlockBehaviour.class.getDeclaredField("isRandomlyTicking");
-            long offset = theUnsafe.objectFieldOffset(f);
-            theUnsafe.putBoolean(DRIED_KELP_BLOCK, offset, true);
-        }
-        catch (NoSuchFieldException e)
-        {
-            throw new RuntimeException("Could not find Field");
-        }
-    }
+public class BlocksMixin {
+	@Inject(method = "<clinit>", at = @At("TAIL"))
+	private static void postStaticInit(CallbackInfo ci) {
+		Unsafe theUnsafe = TheUnsafeHelper.getTheUnsafe();
+		Block block = Blocks.DRIED_KELP_BLOCK;
+		Class<?> clazz = Block.class;
+		while (clazz != Object.class) {
+			try {
+				Field f = clazz.getDeclaredField(ObfuscationReflectionHelper.remapName(INameMappingService.Domain.FIELD, "isRandomlyTicking"));
+				long offset = theUnsafe.objectFieldOffset(f);
+				// this should always fail
+				// but I'm leaving it for the case that there's some JVM where it works
+				// this try-catch can be commented if you want tho
+				try {
+					// maybe some JVM is poorly made in such away that this works
+					// idk
+					f.setAccessible(true);
+					f.setBoolean(block, true);
+					break;
+				} catch (Throwable ignored) {
+				}
+				theUnsafe.putBoolean(clazz.cast(block), offset, true);
+				break;
+			} catch (Throwable e) {
+				if (clazz == clazz.getSuperclass())
+					throw new RuntimeException("Could not find Field");
+				clazz = clazz.getSuperclass();
+				e.printStackTrace();
+			}
+		}
+	}
 }
