efa6dd307b7df349a9ce712c62e6b03c0e4c6eee
andrew0030 <36803991+andrew0030@users.noreply.github.com>
2022-12-23 23:06:42 +0200

    Update 1.19-1.1.4 Fabric
    - Altered Flammable Block registration to support Quilt
    - Altered Compostable Block registration to support Quilt

--- src/main/java/andrews/swampier_swamps/registry/SSFlammables.java
+++ src/main/java/andrews/swampier_swamps/registry/SSFlammables.java
@@ -1,13 +1,11 @@
 package andrews.swampier_swamps.registry;
 
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.FireBlock;
+import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
 
 public class SSFlammables
 {
     public static void registerFlammables()
     {
-        FireBlock fireblock = (FireBlock) Blocks.FIRE;
-        fireblock.setFlammable(SSBlocks.SWAMP_VINE, 15, 100);
+        FlammableBlockRegistry.getDefaultInstance().add(SSBlocks.SWAMP_VINE, 15, 100);
     }
 }
