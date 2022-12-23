0c90e9b9370537ab05e87f2ea75412dc850d60b6
andrew0030 <36803991+andrew0030@users.noreply.github.com>
2022-12-23 23:51:23 +0200

    Update 1.19.3-2.0.2 Fabric
    - Altered Flammable Block registration to support Quilt
    - Altered Compostable Block registration to support Quilt

--- src/main/java/andrews/swampier_swamps/registry/SSCompostable.java
+++ src/main/java/andrews/swampier_swamps/registry/SSCompostable.java
@@ -1,15 +1,15 @@
 package andrews.swampier_swamps.registry;
 
-import net.minecraft.world.level.block.ComposterBlock;
+import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
 
 public class SSCompostable
 {
     public static void registerCompostable()
     {
-        ComposterBlock.COMPOSTABLES.put(SSBlocks.SWAMP_VINE.asItem(), 0.5F);
-        ComposterBlock.COMPOSTABLES.put(SSBlocks.CATTAIL.asItem(), 0.5F);
-        ComposterBlock.COMPOSTABLES.put(SSBlocks.BIG_LILY_PAD.asItem(), 0.85F);
-        ComposterBlock.COMPOSTABLES.put(SSBlocks.SMALL_LILY_PAD.asItem(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(SSBlocks.DECAYING_KELP.asItem(), 0.5F);
+        CompostingChanceRegistry.INSTANCE.add(SSBlocks.SWAMP_VINE.asItem(), 0.5F);
+        CompostingChanceRegistry.INSTANCE.add(SSBlocks.CATTAIL.asItem(), 0.5F);
+        CompostingChanceRegistry.INSTANCE.add(SSBlocks.BIG_LILY_PAD.asItem(), 0.85F);
+        CompostingChanceRegistry.INSTANCE.add(SSBlocks.SMALL_LILY_PAD.asItem(), 0.3F);
+        CompostingChanceRegistry.INSTANCE.add(SSBlocks.DECAYING_KELP.asItem(), 0.5F);
     }
 }
